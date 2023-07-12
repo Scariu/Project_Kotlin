65e615f44184fadcf2a3396fae00158485574e39
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-11 22:17:22 -0400

    Se crea función para convertir temperaturas.

--- app/src/main/java/com/example/project_kotlin/Main.kt
+++ app/src/main/java/com/example/project_kotlin/Main.kt
@@ -1,5 +1,23 @@
 package com.example.project_kotlin
 
 fun main(){
-    
+    println("Ingresar el valor en Celsius : ")
+    val input = readLine()!!.toDouble()
+
+    val celciusToFarenheint = (9.0/5.0) * (input) + 32
+
+    println("De $input° Celcius a Farenheint equivale a $celciusToFarenheint grados Farenheit.")
+
+    val celciuToKelvin = input + 273.15
+
+    println("De $input° Celcius a Kelvin equivale a $celciuToKelvin grados Kelvin.")
+
+    val farenheintToKelvin = (5.0/9.0) * (celciusToFarenheint - 32) + 273.15
+    println("De $celciusToFarenheint° Farenheint  a Kelvin equivale $farenheintToKelvin grados Kelvin.")
+
+
+}
+
+fun isLetters(string: String): Boolean {
+    return string.none { it !in 'A'..'Z' && it !in 'a'..'z' }
 }
